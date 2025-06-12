33b1f03b87b533828cb386897054358023cee11a
Swift0101 <godmaymay3@gmail.com>
2025-06-12 19:58:58 +0700

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,5 +1,5 @@
 local ESP_LUA_URL    = "
-local FULL_LUA_URL   =" https://raw.githubusercontent.com/ThundarZ/Welcome/main/Main/GaG/Main.lua"
+local FULL_LUA_URL   ="
 local GITHUB_KEY_URL = "https://raw.githubusercontent.com/Swift0202/grow-a-garden-1/refs/heads/main/tdata.lua"
 local PLAYER_LIST_URL = "https://raw.githubusercontent.com/Swift0202/grow-a-garden-1/refs/heads/main/playyer.lua"
 
